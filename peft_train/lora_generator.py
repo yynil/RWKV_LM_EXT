8b61b4ceab4b1b53834ec5a136d40d9454a82e10
yynil <yueyu.lin@me.com>
2024-05-17 08:25:39 +0800

    use new example

--- peft_train/lora_generator.py
+++ peft_train/lora_generator.py
@@ -1,5 +1,5 @@
 ckpt = '/media/yueyulin/KINGSTON/models/rwkv6/RWKV-x060-World-1B6-v2.1-20240328-ctx4096.pth'
-lora_file = '/media/yueyulin/data_4t/models/lora/epoch_0/RWKV-x060-World-1B6-v2.1-20240328-ctx4096.pth.pth'
+lora_file = '/media/yueyulin/KINGSTON/tmp/lora/max_batch/20240516-231727/trainable_model/epoch_1_step_40000/RWKV-x060-World-1B6-v2.1-20240328-ctx4096.pth.pth'
 tokenizer_file = '/home/yueyulin/github/RWKV_LM_EXT/tokenizer/rwkv_vocab_v20230424.txt'
 target_modules = ['emb','ffn.key','ffn.value','ffn.receptance','att.key','att.value','att.receptance']
 lora_r = 8
@@ -51,17 +51,8 @@ gen_args = PIPELINE_ARGS(temperature = 1, top_p = 0.96, top_k = 20, # top_k = 0 
                         chunk_len = 512)
 cat_char = '🐱'
 bot_char = '🤖'
-instruction ='根据给定的短文，用最简洁的语言回答以下问题：PCIe成为新的个人电脑主板标准的最主要的原因是什么?'
-input_text = """应用与前景
-技嘉GV-NX62TC256D8显卡，采用PCI Express x16插槽
-
-在2005年，PCIe已近乎成为新的个人电脑主板标准。关于此有不少评论，但最基本的原因是它对于软件开发者完全透明——为PCI所设计的操作系统可以不做任何代码修改来启动PCIe设备。其二，它能增强系统性能，还有强有力的品牌认知。各类网卡、声卡、显卡，以及当下的NVMe固态硬盘都使用了PCIe标准。下面为主流的使用PCIe 的外设产品。
-显卡
-
-大部分新型的AMD或NVIDIA显卡都使用PCIe标准。NVIDIA在它新开发的SLI上采用PCIe的高速数据传输，这使得两块相同芯片组显卡可同时工作于一台电脑之上。AMD公司也基于PCIe开发一种两个GPU一同运作的技术，称为CrossFire。
-硬盘
-当下主流的固态硬盘接口有M.2、U.2、PCIe、SATA、SATA Express、SAS等。M.2和U.2可选PCIe接口[14]。NVMe协议是目前最高效的PCIe SSD协议标准。  
-"""
+instruction ='根据给定的短文，回答以下问题：黄循财的是哪国人？'
+input_text = '黄循财（英语：Lawrence Wong Shyun Tsai，1972年12月18日—），新加坡华裔政治人物，现任新加坡总理兼财政部部长、人民行动党社区基金会主席。他与王乙康和颜金勇共同主持了因应新加坡2019冠状病毒病大流行的多部委工作组。曾任新加坡副总理，教育部、国家发展部、文化、社区及青年部的部长，通讯及新闻部和财政部的第二部长，以及人民行动党副秘书长。[1]黄循财是人民行动党第四代领导层，也是人民行动党中央执行委员会首任副秘书长兼政策论坛顾问。'
 ctx = f'{cat_char}:{instruction}\n{input_text}\n{bot_char}:'
 print(ctx)
 from tokenizer.rwkv_tokenizer import TRIE_TOKENIZER
