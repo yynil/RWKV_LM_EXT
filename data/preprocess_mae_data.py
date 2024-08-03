dafc892b56bfa97e63d3eeb43eed0490c9e13bbd
yynil <yueyu.lin@me.com>
2024-08-03 22:21:34 +0800

    chore: Reduce num_proc parameter in preprocess_mae_data.py
    Optimize performance by reducing the `num_proc` parameter in the `preprocess_mae_data.py` file from 16 to 4 in the `create_wiki_dataset` function.

--- data/preprocess_mae_data.py
+++ data/preprocess_mae_data.py
@@ -226,7 +226,7 @@ def create_wiki_dataset(wiki_dir,
         return {'token_ids': blocks}
     wiki = ds.map(sentence_wiki, num_proc=16, remove_columns=["title", "text"])
     tokenized_wiki = wiki.map(wiki_tokenize_function, num_proc=4, batched=True, remove_columns=["sentences"])
-    processed_wiki = tokenized_wiki.map(wiki_pad_each_line, num_proc=8, batched=True, remove_columns=tokenized_wiki.column_names)
+    processed_wiki = tokenized_wiki.map(wiki_pad_each_line, num_proc=4, batched=True, remove_columns=tokenized_wiki.column_names)
     return processed_wiki
 def creat_book_dataset(book_dir,
                        tokenizer_file,
